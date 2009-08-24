d3b5dfceae3264c4a60c3e855454d8d27b01521d
Cheng-Lung Sung <clsung@gmail.com>
2009-08-24 15:17:45 +0800

    - :p forget

--- lib/Net/Plurk.pm
+++ lib/Net/Plurk.pm
@@ -1,8 +1,19 @@
 package Net::Plurk;
+use Moose;
+use WWW::Mechanize;
+use Net::Plurk::Parse;
+use Net::Plurk::Page;
 
-use warnings;
-use strict;
+use namespace::autoclean;
 
+has url => ( isa => 'Str', is => 'rw', default => 'http://plurk.com' );
+has baseurl => ( isa => 'Str', is => 'ro', default => 'http://plurk.com' );
+has mech => ( is => 'ro', default => sub {WWW::Mechanize->new} );
+has parser => ( isa => 'Net::Plurk::Parse', is => 'ro', lazy_build => 1);
+has pages => ( isa => 'HashRef[Str]', is => 'rw', default => sub { {} });
+has users => ( isa => 'HashRef[Str]', is => 'rw', default => sub { {} });
+
+our $user_pattern = 'plurk.com/user/';
 =head1 NAME
 
 Net::Plurk - The great new Net::Plurk!
@@ -15,7 +26,6 @@ Version 0.01
 
 our $VERSION = '0.01';
 
-
 =head1 SYNOPSIS
 
 Quick summary of what the module does.
@@ -24,7 +34,7 @@ Perhaps a little code snippet.
 
     use Net::Plurk;
 
-    my $foo = Net::Plurk->new();
+    my $foo = Net::Plurk->new( username => 'foo', password => 'bar');
     ...
 
 =head1 EXPORT
@@ -34,18 +44,85 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =head1 FUNCTIONS
 
-=head2 function1
+=head2 set
+
+Set the attributes
 
 =cut
 
-sub function1 {
+sub set {
+    my ($self, %args) = @_;
+    $self->url($args{url}) if $args{url};
 }
 
-=head2 function2
+=head2 get
+
+Get the request url 
 
 =cut
 
-sub function2 {
+sub get {
+    my ($self, $url) = @_;
+    $url = $self->url unless $url;
+    $self->url($url) if ($url ne $self->url);
+    $self->mech->get($self->url);
+    return unless $self->mech->success;
+    $self->parser->content($self->mech->content);
+    my $page = $self->parser->parse();
+    $self->pages->{$self->url} = $page;
+    $self->users->{$page->author->username} = $page->author if $page->author;
+    return $self->pages->{$self->url}->title;
+}
+
+=head2 goto
+
+=cut
+
+sub goto {
+    my ($self, %args) = @_;
+    $self->set(%args);
+    return $self->get();
+}
+
+=head2 get_user
+
+=cut
+
+sub get_user {
+    my ($self, $user, $renew) = @_;
+    return $self->users->{$user} unless ($renew or !($self->users->{$user}));
+    my $url = $self->baseurl."user/".$user;
+    $self->get($url);
+    return $self->users->{$user};
+}
+
+=head2 get_title
+
+=cut
+
+sub get_title {
+    my ($self, $url) = @_;
+    $url = $self->url unless $url;
+    return $self->pages->{$url}->title;
+}
+
+=head2 get_karma
+
+=cut
+
+sub get_karma {
+    my ($self, %args) = @_;
+    return 0 unless $args{user};
+    return $self->get_user($args{user},$args{renew})->karma;
+}
+
+=head2 _build_parser
+
+=cut
+
+sub _build_parser {
+    my $self = shift;
+    return Net::Plurk::Parse->new();
 }
 
 =head1 AUTHOR
@@ -104,4 +181,6 @@ under the same terms as Perl itself.
 
 =cut
 
+no Moose;
+__PACKAGE__->meta->make_immutable;
 1; # End of Net::Plurk
