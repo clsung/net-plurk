251b7c11614c62bf3a31f3a3ab083815b6a34412
Cheng-Lung Sung <clsung@gmail.com>
2009-08-24 10:12:59 +0800

    - init via module-starter

---
+++ lib/Net/Plurk.pm
@@ -0,0 +1,107 @@
+package Net::Plurk;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Net::Plurk - The great new Net::Plurk!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Plurk;
+
+    my $foo = Net::Plurk->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Cheng-Lung Sung, C<< <clsung at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-plurk at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Plurk>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Plurk
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Plurk>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-Plurk>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-Plurk>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-Plurk/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Cheng-Lung Sung, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Net::Plurk
