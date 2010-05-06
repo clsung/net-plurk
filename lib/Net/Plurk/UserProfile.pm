787dd9873db5c2243a9b8f1dd927ff1c356bb837
Cheng-Lung Sung <clsung@gmail.com>
2010-05-06 11:20:25 +0800

    - use Maybe[Object] instead of Maybe[JSON::Boolean]

--- lib/Net/Plurk/UserProfile.pm
+++ lib/Net/Plurk/UserProfile.pm
@@ -30,18 +30,16 @@ has 'privacy' => (is => 'ro', isa => enum([qw[ world only_friends only_me ]]));
 #has 'plurks' => (is => 'ro', isa => 'ArrayRef[Net::Plurk::PlurkContent]');
 has 'plurks' => (is => 'ro', isa => 'ArrayRef');
 
+no Moose::Util::TypeConstraints;
 package Net::Plurk::PublicUserProfile;
-use Moose::Util::TypeConstraints;
 use Moose;
 
 extends 'Net::Plurk::UserProfile';
 
-#subtype 'JSON::XS::Boolean' => as class_type('JSON::Boolean');
-has 'are_friends' => (is => 'ro', isa => 'Maybe[JSON::XS::Boolean]', default => 'JSON::false');
-has 'is_fan' => (is => 'ro', isa => 'Maybe[JSON::XS::Boolean]', default => 'JSON::false');
-has 'is_following' => (is => 'ro', isa => 'Maybe[JSON::XS::Boolean]', default => 'JSON::false');
+has 'are_friends' => (is => 'ro', isa => 'Maybe[Object]', default => 'JSON::false');
+has 'is_fan' => (is => 'ro', isa => 'Maybe[Object]', default => 'JSON::false');
+has 'is_following' => (is => 'ro', isa => 'Maybe[Object]', default => 'JSON::false');
 
-no Moose::Util::TypeConstraints;
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
