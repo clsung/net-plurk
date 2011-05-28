1051ad40005c16944a789be7ed49bf29db0f013e
Cheng-Lung Sung <clsung@gmail.com>
2011-05-28 11:02:42 +0800

    seems the returned data is not like what API document says

--- lib/Net/Plurk/User.pm
+++ lib/Net/Plurk/User.pm
@@ -12,20 +12,21 @@ Foobar
 =cut
 
 has 'id' => (is => 'rw', isa => 'Int');
-has 'uid' => (is => 'rw', isa => 'Int');
 has 'has_profile_image' => (is => 'rw', isa => 'Int');
 has 'avatar' => (is => 'rw', isa => 'Maybe[Int]');
 # not_saying, single, married, divorced, engaged, in_relationship, complicated, widowed, open_relationship
 has 'relationship' => (is => 'rw', isa => 'Str');
 has 'full_name' => (is => 'rw', isa => 'Str');
 has 'nick_name' => (is => 'rw', isa => 'Str');
-has 'display_name' => (is => 'rw', isa => 'Str', lazy_build => 1);
+has 'display_name' => (is => 'rw', isa => 'Str', lazy_build => 1); # XXX
 has 'location' => (is => 'rw', isa => 'Str');
 has 'timezone' => (is => 'rw', isa => 'Any');
 has 'date_of_birth' => (is => 'rw', isa => 'Maybe[Str]');
 has 'karma' => (is => 'rw', isa => 'Num', default => 0);
 has 'gender' => (is => 'rw', isa => 'Int');
-has 'recruited' => (is => 'rw', isa => 'Int');
+has 'recruited' => (is => 'rw', isa => 'Int'); # XXX
+has 'is_premium' => (is => 'ro', isa => 'Maybe[Object]', default => 'JSON::false');
+has 'email_confirmed' => (is => 'ro', isa => 'Maybe[Object]', default => 'JSON::false');
 
 sub _build_display_name {
     my $self = shift;
